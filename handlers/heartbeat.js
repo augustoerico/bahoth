14fd4be79aec0c5127708964efdb3ebb1f1d52e6
Erico Silva <augusto.ericosilva@gmail.com>
2018-08-24 21:32:15 -0400

    Fix heartbeat status code

--- handlers/heartbeat.js
+++ handlers/heartbeat.js
@@ -1,6 +1,6 @@
 module.exports.handler = async (event) => {
     return {
-        statusCode: 201,
+        statusCode: 200,
         body: JSON.stringify({
             message: 'Heartbeat',
             input: event
